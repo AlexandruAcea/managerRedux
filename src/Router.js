1438a4ba50e1215a2e9d92409111da591a4cbaa8
AlexandruAcea <alex.acea98@gmail.com>
2019-03-20 12:20:09 +0200

    switching to Gram

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Scene, Router } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
 import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
+import { Actions } from "react-native-router-flux";
 
 const RouterComponent = () => {
   return (
@@ -17,9 +19,20 @@ const RouterComponent = () => {
         </Scene>
         <Scene key="main">
           <Scene
+            onRight={() => {
+              Actions.employeeCreate();
+            }}
+            rightTitle="Add"
             key="employeeList"
             component={EmployeeList}
-            title="Employee List"
+            title="Employee"
+            initial
+          />
+
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
           />
         </Scene>
       </Scene>
